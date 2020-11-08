3fbd9f5eca3c8631ca4ce0964fcffaee3d45c9f0
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2020-11-08 18:42:43 +0200

    Add files via upload

---
+++ Problema1_FOR.py
@@ -0,0 +1,3 @@
+#Utilizînd ciclul FOR elaboraţi un program care afişează primele 10 numere naturale şi păstrarea lor în ordine crescătoare.
+for i in range(1,11):
+    print('Primele 10 numere naturale în ordine crescătoare sunt: ',i, end=',')
---
+++ Problema6_FOR.py
@@ -0,0 +1,50 @@
+'''Să se calculeze sumele: 	s1=1+2+3+…+n
+							s2=1*2+2*3+3*4+…+(n-1)*n
+							s3=1+1*2+1*2*3+…+1*2*3*…*n
+							s4=12+22+32+…+n2
+							s5=1/2+2/3+3/4+…+n/(n+1)
+							s6=1+2+22+23+24+…+2n
+							Notă: Pentru fiecare sumă n – se va citi de la tastatură.'''
+s1 = 0
+n = eval(input("n = "))
+for i in range(1,n+1):
+
+	s1+=i
+print('suma este ', s1)
+
+s2 = 0
+n = eval(input("n = "))
+for i in range(2,n+1):
+	s2+=(i-1)*i
+print('suma este ', s2)
+
+s3 = 0
+n = eval(input("n = "))
+p = 1
+for i in range(1,n+1):
+	p*=i
+	s3+=p	
+print('suma este ', s3)
+
+s4 = 0
+n = eval(input("n = "))
+for i in range(12,n*10+3,10):
+	s4+=i
+print('suma este ', s4)
+
+s5 = 0
+n = eval(input("n = "))
+for i in range(1,n+1):
+    s5+=i/(i+1)
+print('suma este ', s5)
+
+n = eval(input("n = "))
+s6 = 3
+for i in range(2,n+1):
+	z = 0  # nr. de cifre a nr. i
+	k = i  # memoram valoarea lui i in k
+	while k!=0: #cit timp nr. k != 0 
+		k//=10 # aflam citul
+		z+=1   # incrementam nr. de cifre
+	s6+=2*10**z+i # aduagam 2*10^z+i la suma	
+print('suma este ', s6)
